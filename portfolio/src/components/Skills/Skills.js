c1434e6c16931b8f322cd79976160d743b9b9ec3
Kadeejath Salaha <codestudy475@gmail.com>
2024-10-01 18:17:48 +0530

    skillset

--- portfolio/src/components/Skills/Skills.js
+++ portfolio/src/components/Skills/Skills.js
@@ -4,20 +4,19 @@ import { Container, Row, Col, ProgressBar } from "react-bootstrap";
 
 const skillsData = [
   { name: "React.js", level: 70 },
-  { name: "Node.js", level: 50 },
+  { name: "Node.js", level: 55 },
   { name: "HTML/CSS/JS", level: 95 },
   { name: "Bootstrap/MUI", level: 65 },
   { name: "MySQL", level: 80 },
   { name: "Git", level: 80 },
   { name: "C++", level: 85 },
-  { name: "Java", level: 48 },
+  { name: "Python", level: 50 },
+  { name: "Java", level: 40 },
   { name: "MongoDB", level: 35 },
-  { name: "Android Studio", level: 50 },
-  { name: "C#", level: 30 },
-  { name: "Python", level: 48 },
-  {name:"RStudio", level:25},
+  { name: "Android Studio", level: 30 },
+  { name: "ASP.NET", level: 20 },
   {name:"Problem Solving", level:78},
-  {name:"Firebase", level:30},
+  {name:"Firebase", level:20},
 ];
 
 function Skills() {
