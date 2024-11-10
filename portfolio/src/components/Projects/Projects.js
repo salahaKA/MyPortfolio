e5de7b12e6c594fd472b984ff050a5898a7c174e
Kadeejath Salaha <codestudy475@gmail.com>
2024-11-10 11:54:56 +0530

    nov commit

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ import portfolio from "../../Assets/Projects/portfolio.png";
 import Eigo from "../../Assets/Projects/eigo.png";
 import eigo_shoppie from "../../Assets/Projects/Eigo1.png";
 import DCS from "../../Assets/Projects/DCS.png";
-import penGes from "../../Assets/Projects/penGes3.png";
+import penGes from "../../Assets/Projects/penGes1.png";
 import BE from "../../Assets/Projects/BE.png";
 import WeCare from "../../Assets/Projects/weCare.png";
 import Epitom23 from "../../Assets/Projects/Epitom.png";
@@ -46,11 +46,12 @@ function Projects() {
               imgPath={eigo_shoppie}
               isBlog={false}
               title="Eigo Shopping Cart (2024)"
-              description="Designed responsive shopping cart website using React technology"
+              description="Shopping cart static website using React technology"
               ghLink="https://github.com/salahaKA/eigo_shoppingweb.git"
               demoLink="https://salahaka.github.io/eigo_shoppingweb/"
             />
           </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Epitom23}
@@ -58,7 +59,7 @@ function Projects() {
               title="Diabetes Prediction Using Different Machine Learning Algorithm (2023)"
               description="AIMIT 'SACAIM'23 INTERNATIONAL CONFERENCE ON ADVANCE 
 IT, ENGINEERING AND MANAGEMENT'  research based project done with SIG team"
-              ghLink=""
+              ghLink="https://github.com/salahaKA/Diabetes-Analysis-Using-Machine-Learning-Algorithms.git"
               demoLink="https://chapters.redshine.in/index.php/redshine/article/view/1838"
               pdfLink={SACAIM}
             />
@@ -69,7 +70,8 @@ IT, ENGINEERING AND MANAGEMENT'  research based project done with SIG team"
               imgPath={BE}
               isBlog={false}
               title="Bharath Enterprises (AIMIT BCP - 2024)"
-              description="Built an E-commerce hardware store web application with Django for managing online sales and payments with SIG team."
+              description="An E-commerce hardware store web application to manage online sales and
+ payments built with the AIMIT SIG team using Python Django technology."
               ghLink="https://github.com/salahaKA/BharathEnterprises-Django.git"
               demoLink=""
             />
@@ -80,7 +82,7 @@ IT, ENGINEERING AND MANAGEMENT'  research based project done with SIG team"
               imgPath={penGes}
               isBlog={false}
               title="penGes (AIMIT mini - 2023)"
-              description="Created an Android notes application using Java, XML and Firebase for efficient note-taking and organization."
+              description="Android fullstack notes application using Java, XML and Firebase for efficient note-taking and organization."
               ghLink="https://github.com/salahaKA/penGes_android_app.git"
               demoLink=""
             />
@@ -91,7 +93,8 @@ IT, ENGINEERING AND MANAGEMENT'  research based project done with SIG team"
               imgPath={WeCare}
               isBlog={false}
               title="We Care (BSC final - 2022)"
-              description="Palliative care Magement sysytem using HTML, CSS, JS, bootstrap, MySQl and Python flask technology"
+              description="A Palliative Care Management System designed to streamline patient care and
+ tracking, built using HTML/CSS/JS, bootstrap, Python Flask and MySQL."
               ghLink="https://github.com/salahaKA/wecare"
               demoLink=""
               pdfLink={pdfWecare}
@@ -133,7 +136,7 @@ IT, ENGINEERING AND MANAGEMENT'  research based project done with SIG team"
               imgPath={Eigo}
               isBlog={false}
               title="UI/UX"
-              description="Demo shopping website frontend design home"
+              description="Demo shopping website home page"
               ghLink="https://github.com/salahaKA/Eigo_shoppingCart"
               demoLink="https://salahaka.github.io/Eigo_shoppingCart/"
             />
